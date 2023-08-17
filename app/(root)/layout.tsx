27aa0168059b5602f5782ce62289b6d15126248b
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-17 18:04:39 +0000

    update navbar and wrap clerk provider in root too

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,18 +21,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Topbar />
-        <main>
-          <LeftSidebar />
-          <section className="main-container">
-            <div className="w-full max-w-4xl">{children}</div>
-          </section>
-          <RightSidebar />
-        </main>
-        <Bottombar />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
