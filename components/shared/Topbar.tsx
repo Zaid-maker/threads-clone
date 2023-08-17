20279b5e1d3312634aa957f7b92e7d6e77f1beb5
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-17 17:30:05 +0000

    start working on topbar component and add assets

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-const Topbar = () => {
-  return <div>Topbar</div>;
-};
+function Topbar() {
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+    </nav>
+  );
+}
 
 export default Topbar;
