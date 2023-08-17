f3f9456c0bc322d488303608d20f664bbfd65eab
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-17 14:53:41 +0000

    add auth routes and custom layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import React from "react";
+
+import "../globals.css";
+
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Threads clone application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
