55d08973a2244f70fcc7f54808491c4e3a573806
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-17 15:21:03 +0000

    UserButton is not working for some reason

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
+      <h1>Threads</h1>
     </div>
   );
 }
