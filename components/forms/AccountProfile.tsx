73449759849c7afb0261331072d68055509ec6c7
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-18 16:00:06 +0000

    start working on onboarding

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
+function AccountProfile({ user, btnTitle }: Props) {
+  return <div className="text-light-2">Account Profile</div>;
+}
+
+export default AccountProfile;
