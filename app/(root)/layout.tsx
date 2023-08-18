0ef9a7db7f6f2e0ab33861a31b870de859959598
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-18 13:28:12 +0000

    complete the rightsidebar and fix layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <Topbar />
-          <main>
+          <main className="flex  flex-row">
             <LeftSidebar />
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import React from "react";
 
-const RightSidebar = () => {
-  return <div>RightSidebar</div>;
-};
+function RightSidebar() {
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Communities
+        </h3>
+      </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+      </div>
+    </section>
+  );
+}
 
 export default RightSidebar;
