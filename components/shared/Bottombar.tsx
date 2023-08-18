a09d402f92f5714f15bf8aceaf57ee504e246128
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-18 13:18:32 +0000

    complete the bottom bar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,7 +1,45 @@
+"use client";
+
+import { useRouter, usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Bottombar = () => {
-  return <div>Bottombar</div>;
-};
+function Bottombar() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.route}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
 
 export default Bottombar;
