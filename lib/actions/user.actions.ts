7f1e7806c835812c9092cbc4d116a57c76eee37d
DevMirza <pzhafeez@gmail.com>
2023-08-20 16:09:16 +0000

    style fetchUser

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,15 +1,18 @@
-'use server'
+"use server";
 
 import Community from "../models/community.model";
 import User from "../models/user.model";
+import { connectDB } from "../mongoose";
 
 export async function fetchUser(userId: string) {
-    try {
-        return await User.findOne({ id: userId }).populate({
-            path: "communities",
-            model: Community,
-        });
-    } catch (error: any) {
-        throw new Error(`Failed to fetch user: ${error.message}`);
-    }
+  try {
+    connectDB();
+
+    return await User.findOne({ id: userId }).populate({
+      path: "communities",
+      model: Community,
+    });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
+  }
 }
