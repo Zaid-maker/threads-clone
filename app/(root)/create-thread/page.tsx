b017dc940c28cf00568e72cf4f37659974671341
DevMirza <pzhafeez@gmail.com>
2023-08-21 16:09:28 +0000

    working on create thread feature

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,24 @@
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+async function Page() {
+  const user = currentUser();
+
+  if (!user) return null;
+
+  // @ts-ignore
+  const userInfo = await fetchUser(user.id);
+
+  // if (!userInfo?.onboarded) redirect("/onboarding");
+
+  return (
+    <>
+      <h1 className="head-text">Create Thread</h1>
+
+      <PostThread userId={userInfo._id} />
+    </>
+  );
+}
+
+export default Page;
