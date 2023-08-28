b31bb7c09e3b85661f5b407a927780f6d4ae6c6d
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-28 15:45:43 +0000

    add deleteThread action and added to DeleteTHread

--- components/forms/DeleteThread.tsx
+++ components/forms/DeleteThread.tsx
@@ -1,4 +1,6 @@
+import { deleteThread } from "@/lib/actions/thread.actions";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   threadId: string;
@@ -15,6 +17,11 @@ function DeleteThread({
   parentId,
   isComment,
 }: Props) {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  if (currentUserId !== authorId || pathname === "/") return null;
+
   return (
     <Image
       src="/assets/delete.svg"
@@ -22,6 +29,13 @@ function DeleteThread({
       width={18}
       height={18}
       className="cursor-pointer object-contain"
+      onClick={async () => {
+        await deleteThread(JSON.parse(threadId), pathname);
+
+        if (!parentId || !isComment) {
+          router.push("/");
+        }
+      }}
     />
   );
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -37,6 +37,14 @@ export async function createThread({
   }
 }
 
+export async function deleteThread(id: string, path: string): Promise<void> {
+  try {
+    connectDB()
+  } catch (error: any) {
+    throw new Error(`Failed to delete thread: ${error.message}`)
+  }
+}
+
 export async function fetchPosts(pageNumber = 1, pageSize = 20) {
   connectDB();
 
