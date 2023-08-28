4c9cd484660b9c2860dac1463b09d806a66cd820
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-28 10:28:26 +0000

    add styling to THread Card

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -42,12 +42,19 @@ function ThreadCard({
         isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
       }`}
     >
-      <div>
-        <div>
-          <div>
-            <Link href={`/profuke/${author.id}`}>
-              <Image src={author.image} alt="user_community_image" fill />
+      <div className="flex items-start justify-between">
+        <div className="flex w-full flex-1 flex-row gap-4">
+          <div className="flex flex-col items-center">
+            <Link href={`/profuke/${author.id}`} className="relative h-11 w-11">
+              <Image
+                src={author.image}
+                alt="user_community_image"
+                fill
+                className="cursor-pointer rounded-full"
+              />
             </Link>
+
+            <div className="thread-card_bar" />
           </div>
         </div>
       </div>
