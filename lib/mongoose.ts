bbdd6dea8042ad87048412f01429a72399b5c361
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-28 07:44:50 +0000

    add tailwindcss extension in devcontainer

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -6,7 +6,10 @@ export const connectDB = async () => {
   mongoose.set("strictQuery", true);
 
   if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
-  if (isConnected) return console.log("Already connected to MongoDB");
+  if (isConnected) {
+    console.log("MongoDB connection already established");
+    return;
+  }
 
   try {
     await mongoose.connect(process.env.MONGODB_URL);
